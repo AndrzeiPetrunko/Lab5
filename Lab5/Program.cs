fdf560cb8079779e219b95b644b6282c346f9fbe
Andrii <andriypetrnko@gmail.com>
2024-01-11 17:31:54 +0100

    Add project files.

---
+++ Lab5/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Lab5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Zadanie1();
+        }
+        public static void Zadanie1()
+        {
+            Console.WriteLine(1);
+        }
+    }
+}
